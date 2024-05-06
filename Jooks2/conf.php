1ce640afb183d19da879d2a0f285110fe896d8e1
Deniss Gorjunov <pinkod2222@gmail.com>
2024-05-06 10:39:58 +0300

    DB connect

--- Jooks2/conf.php
+++ Jooks2/conf.php
@@ -1,7 +1,7 @@
 <?php
-$kasutaja='d123187_maksimts';
-$serverinimi='d123187.mysql.zonevs.eu';
-$parool='0b3z8OhUXNCt';
-$andmebaas='d123187_database';
+$kasutaja='root';
+$serverinimi='localhost';
+$parool='';
+$andmebaas='jooksvoistlus';
 $yhendus=new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
 $yhendus->set_charset('UTF8');
